3947d39bae5c784948c2b6fd6dd5c79f804774bd
JaredLove <lovejared91@Yahoo.com>
2024-07-12 18:55:20 -0500

    added a form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-const buttonEl = document.querySelector("#save-task");
+const formEl = document.querySelector("#task-form");
 const taskToDoEl = document.querySelector("#task-to-do")
 
-const createTaskHandler = function(){
-  alert("This was started");
+const createTaskHandler = function(event){
+  event.preventDefault();
   const taskItemEl = document.createElement("li");  
   taskItemEl.className = "task-item";
   taskItemEl.textContent="hello";
   taskToDoEl.appendChild(taskItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler); 
+formEl.addEventListener("submit", createTaskHandler); 
