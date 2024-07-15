d42ba34f6f0318ab0fab02100886db01af57f11d
JaredLove <lovejared91@Yahoo.com>
2024-07-15 11:17:37 -0500

    added submit logic for user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
 const formEl = document.querySelector("#task-form");
-const taskToDoEl = document.querySelector("#task-to-do")
+const tasksToDoEl = document.querySelector("#tasks-to-do");
+
 
 const createTaskHandler = function(event){
   event.preventDefault();
-  const taskItemEl = document.createElement("li");  
-  taskItemEl.className = "task-item";
-  taskItemEl.textContent="hello";
-  taskToDoEl.appendChild(taskItemEl);
-}
+  const taskNameInput = document.querySelector("input[name='task-name']").value;
+  const taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  const listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+
+  const taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML = `<h3 class='task-name'> ${taskNameInput} </h3>
+                          <span class='task-type'> ${taskTypeInput} </span>`;
+
+  
+  listItemEl.appendChild(taskInfoEl);
+  tasksToDoEl.appendChild(listItemEl);
+} 
 
 formEl.addEventListener("submit", createTaskHandler); 
